cb679a2fcd0efb1be4f6bef7cd879157c1d0a3a1
Tomusionis <Tomcikaz@yahoo.com>
2020-09-17 19:55:40 +0000

    add funcion mix to more DRY code

--- script.js
+++ script.js
@@ -4,14 +4,26 @@ let green;
 let blue;
 let randomRGB;
 
-
-for (i = 1; i <= 10; i ++) {
-red = Math.floor(Math.random() * 256);
-green = Math.floor(Math.random() * 256);
-blue = Math.floor(Math.random() * 256);
-randomRGB = `rgb( ${red}, ${green}, ${blue} )`;
-html += `<div style="background-color: ${randomRGB}">${i}</div>`;
-
+function mix() { 
+  return Math.floor(Math.random() * 256);
 }
 
+for (i = 1; i <= 10; i ++) {
+
+  randomRGB = `rgb( ${mix(red)}, ${mix(green)}, ${mix(blue)} )`;
+  html += `<div style="background-color: ${randomRGB}">${i}</div>`;
+  
+  }
+
 document.querySelector('main').innerHTML = html;
+
+
+
+//for (i = 1; i <= 10; i ++) {
+//red = Math.floor(Math.random() * 256);
+//green = Math.floor(Math.random() * 256);
+//blue = Math.floor(Math.random() * 256);
+//randomRGB = `rgb( ${red}, ${green}, ${blue} )`;
+//html += `<div style="background-color: ${randomRGB}">${i}</div>`;
+//
+//}
